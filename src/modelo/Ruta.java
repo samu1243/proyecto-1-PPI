0349848fb0bb60f635975d9bcfd34f4567975e07
Juan Ferreira <juandiego28100@gmail.com>
2026-03-04 23:27:28 -0400

    contenido para el resultado de Dijkstra

--- src/modelo/Ruta.java
+++ src/modelo/Ruta.java
@@ -1,7 +1,21 @@
 /**
  *
- * @author Diego
+ * @author Juan Ferreira, 
+ *      
  */
+
+package modelo;
+
 public class Ruta {
-    
+    private ListaEnlazada camino;
+    private double distancia;
+
+    public Ruta(ListaEnlazada camino, double distancia) {
+        this.camino = camino;
+        this.distancia = distancia;
+    }
+
+    public ListaEnlazada getCamino() { return camino; }
+    public double getDistancia() { return distancia; }
 }
+
